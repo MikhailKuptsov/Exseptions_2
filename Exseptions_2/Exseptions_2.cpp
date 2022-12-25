4586f7e237658b81651529a90fdc3b5219b9a263
mikhail <mikhail.kuptsov.03@mail.ru>
2022-12-25 22:09:49 +0300

    Добавьте файлы проекта.

---
+++ Exseptions_2/Exseptions_2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+
+bool vys(int year)
+{
+    bool res = false;
+    if (year % 4 == 0)
+        res = true;
+    if (year % 100 == 0)
+        res = false;
+    if (year % 400 == 0)
+        res = true;
+    return res;
+}
+int date(int d, int m, int y)
+{
+    int k = d;
+    if (vys(y) && (m > 2))
+        k += y * 366;
+    else k += y * 365;
+    switch (m - 1)
+    {
+    case 12: k += 31;
+    case 11: k += 30;
+    case 10: k += 31;
+    case  9: k += 30;
+    case  8: k += 31;
+    case  7: k += 31;
+    case  6: k += 30;
+    case  5: k += 31;
+    case  4: k += 30;
+    case  3: k += 31;
+    case  2: k += 28;
+    case  1: k += 31;
+    }
+    return k;
+}
+int difference(int day1, int day2, int mon1, int mon2, int year1, int year2)
+{
+    int k = date(day2, mon2, year2) - date(day1, mon1, year1) + 1;
+    return k;
+}
+void main()
+{
+    int d1, d2, m1, m2, y1, y2;
+    cout << "Input first day: ";
+    cin >> d1;
+    cout << "Input first month: ";
+    cin >> m1;
+    cout << "Input first year: ";
+    cin >> y1;
+    cout << "Input second day: ";
+    cin >> d2;
+    cout << "Input second month: ";
+    cin >> m2;
+    cout << "Input second year: ";
+    cin >> y2;
+    cout << "Between these two dates are " << difference(d1, d2, m1, m2, y1, y2) << " days\n";
+}
